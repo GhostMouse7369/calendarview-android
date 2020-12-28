b28a407f30cb47e626a828c7ee416bdc95057f69
laoshuzi <215779374@qq.com>
2020-12-28 15:16:19 +0800

    [add]calendarview 0.1.1

--- calendarview/build.gradle.kts
+++ calendarview/build.gradle.kts
@@ -12,7 +12,7 @@ plugins {
 
 group = "top.laoshuzi.android"
 //version = AndroidBuildConfig.version_name
-version = "0.1.0"
+version = "0.1.1"
 
 val ziweiRepositoryDomainName: String by project
 val ziweiRepositoryUsername: String by project
